248b799bdf22080ca56a8fd4377015ebab44882d
Sanjeeee <113427749+Sanjeeee@users.noreply.github.com>
2023-10-11 14:35:30 +0530

    Add files via upload

---
+++ DAY 1/add.java
@@ -0,0 +1,10 @@
+class add{
+public static int add(int a,int b){
+return a+b;
+}
+public static void main(String [] args){
+int a=10,b=15;
+int sum=add(a,b);
+System.out.println("the sum of the nukmbers="+sum);
+}
+}
---
+++ DAY 1/name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class name{
+public static void main(String [] args){
+Scanner scanner=new Scanner(System.in);
+System.out.println("enter a anme");
+String string = scanner.nextLine();
+System.out.println("name is"+string);
+}
+}
